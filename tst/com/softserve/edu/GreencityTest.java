1e672b571845f8dac6acdad0fe9a140974c9edae
AnnaShliakhina <anna.shliakhina@gmail.com>
2025-01-04 18:56:44 +0200

    Sign up in Greencity

--- tst/com/softserve/edu/GreencityTest.java
+++ tst/com/softserve/edu/GreencityTest.java
@@ -1,63 +1,64 @@
 package com.softserve.edu;
 
-import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
-public class GreencityTest {
-    private static final Long IMPLICITLY_WAIT_SECONDS = 10L;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-    @Test
-    public void checkNews() throws InterruptedException {
+public class GreencityTest {
+
+    private static WebDriver driver;
+    private static WebDriverWait wait;
+
+    @BeforeAll
+    public static void setUp() {
         System.setProperty("webdriver.chrome.driver", "./lib/chromedriver.exe");
-        //
-        WebDriver driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(IMPLICITLY_WAIT_SECONDS)); // 0 by default
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         driver.manage().window().maximize();
-        //
-        driver.get("https://www.greencity.cx.ua/");
-        Thread.sleep(1000); // For Presentation
-        //
-        // Switch to Ggreencity
-        driver.findElement(By.cssSelector("app-header:nth-child(1) .nav-left-list:nth-child(4) > .url-name")).click();
-        Thread.sleep(1000); // For Presentation
-        //
-        // Check Language
-        String lang = driver.findElement(By.cssSelector("ul[aria-label='language switcher'] span")).getText();
-        if (lang.toLowerCase().equals("ua")) {
-            // Open Dropdown Language
-            driver.findElement(By.cssSelector(".header_arrow")).click();
-            Thread.sleep(1000); // For Presentation
-            //
-            // Click EN
-            driver.findElement(By.cssSelector(".ng-star-inserted > span")).click();
-            Thread.sleep(1000); // For Presentation
-        }
-        //
-        driver.findElement(By.cssSelector(".nav-left-list:nth-child(1) > .url-name")).click();
-        Thread.sleep(1000); // For Presentation
-        //
-        driver.findElement(By.cssSelector(".tag-button:nth-child(3) .text")).click();
-        Thread.sleep(1000); // For Presentation
-        //
-        driver.findElement(By.cssSelector(".gallery-view-li-active:nth-child(2) .added-data p")).click();
-        Thread.sleep(1000); // For Presentation
-        //
-        driver.findElement(By.cssSelector(".app-wrapper")).click();
-        Thread.sleep(1000); // For Presentation
-        //
-        driver.findElement(By.cssSelector(".ql-editor")).click();
-        Thread.sleep(1000); // For Presentation
-        //
-        Assertions.assertEquals("Explore the cutting edge of sustainability, climate action, and eco-innovation with Green Horizons. Discover inspiring stories, groundbreaking research, and simple actions that can help reshape our world into a greener, more resilient place for future generations",
-                driver.findElement(By.cssSelector(".ql-editor > p")).getText() );
-        //
-        Thread.sleep(8000); // For Presentation
-        driver.quit();
     }
 
+    @Test
+    public void positiveRegistrationTest() {
+        driver.get("https://www.greencity.cx.ua/");
+        driver.manage().window().setSize(new org.openqa.selenium.Dimension(806, 690));
+
+        // Очікування, поки кнопка реєстрації стане клікабельною
+        WebElement registerButton = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("app-header:nth-child(1) .header_sign-up-link:nth-child(4) span:nth-child(1)")));
+        registerButton.click();
+
+        // Очікування, поки поле електронної пошти стане видимим
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("email")));
+        driver.findElement(By.id("email")).sendKeys("icf28767@msssg.com");
+
+        driver.findElement(By.id("firstName")).sendKeys("Hey1");
+
+        // Очікування, поки поле паролю стане видимим
+        WebElement passwordField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("password")));
+        passwordField.sendKeys("Anna$5685-ANNA");
+
+        driver.findElement(By.id("repeatPassword")).sendKeys("Anna$5685-ANNA");
+
+        // Очікування, поки кнопка відправки стане клікабельною
+        WebElement submitButton = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(".ubsStyle")));
+        submitButton.click();
+
+        // Додайте перевірки для успішної реєстрації
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".modal-header")));
+        assertEquals("Ви успішно зареєструвалися на цьому сайті. Підтвердьте свою адресу електронної пошти у своїй поштовій скриньці", driver.findElement(By.cssSelector(".modal-header")).getText());
+    }
+
+    @AfterAll
+    public static void tearDown() {
+        driver.quit();
+    }
 }
